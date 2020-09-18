cf6f20432cd5e060276318ee3c2ebe000d1761b2
Bavfalcon9 <olybear9@gmail.com>
2020-09-18 17:01:04 -0500

    feat(buf2hex): Adds a utility to convert streams to hex

---
+++ lib/buffer/bufferToHex.ts
@@ -0,0 +1,5 @@
+export default function bufferToHex(buffer: Uint8Array): string {
+     const buffArr: number[] = Array.from(buffer);
+     const arrParts: string[] = buffArr.map((x: number) => ('00' + x.toString(16).slice(-2)));
+     return arrParts.join('');
+}
